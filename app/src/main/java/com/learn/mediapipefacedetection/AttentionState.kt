138cd7a9bf0bb78a86f863aa9254f458fd8b3aba
niharshete001 <52775891+niharshete001@users.noreply.github.com>
2026-07-07 14:37:59 +0530

    Add files via upload

---
+++ app/src/main/java/com/learn/mediapipefacedetection/AttentionState.kt
@@ -0,0 +1,12 @@
+package com.learn.mediapipefacedetection
+
+enum class AttentionState {
+
+    ATTENTIVE,
+    LOOKING_LEFT,
+    LOOKING_RIGHT,
+    LOOKING_UP,
+    LOOKING_DOWN,
+    EYES_CLOSED,
+    NO_FACE
+}
